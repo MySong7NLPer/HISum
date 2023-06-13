dc5e0707551d41961676819b6d8ac661394efebc
M.Song <104239980+MySong7NLPer@users.noreply.github.com>
2023-06-13 12:21:09 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python train_matching.py --mode=train --encoder=bert --save_path=/data/mingyang/hisum --gpus=0,1,2,3,4,5,6,7 --batch_size=16
